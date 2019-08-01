793508f6078ddea799912765a3a49c3713a430ef
kolibril13 <jan-hendrik.mueller@gmx.net>
2019-08-01 11:42:27 +0200

    #a dot tracker that is working

--- hendrik_active/Image_Processing/poisson_noise_and_snr/k_Space/test03_Valuetracker_minimal.py
+++ hendrik_active/Image_Processing/poisson_noise_and_snr/k_Space/test03_Valuetracker_minimal.py
@@ -3,25 +3,22 @@ from manimlib.imports import *
 class lllll(Scene):
     def get_dot(self,num):
         print(num)
-        return Dot().shift(LEFT*num)
+        return Dot().shift(LEFT*0.01 *num)
     def construct(self):
-        dot = Dot()
-        self.add(dot)
-        val_track=ValueTracker(0)
-        do= self.get_dot(val_track.get_value())
-        # do.add_updater(
-        #     lambda x: x.become(self.get_dot(val_track.get_value()))
-        # )
-
-
-
-        self.play(val_track.set_value,1000)
+        dot_tracker= ValueTracker(0)
+        dot_disp= self.get_dot(dot_tracker.get_value())
         
-        self.wait(2)
+        def dot_updater(dot_disp):
+            dot_disp.become(self.get_dot(dot_tracker.get_value()))
 
+        dot_disp.add_updater(dot_updater)
+        self.add(dot_disp)
+        self.play(dot_tracker.set_value, 200, rate_func= linear)
+        self.wait()
+       
     
 if __name__ == "__main__":
     module_name = os.path.basename(__file__)
-    command_A = "manim  -s -c '#2B2B2B' --video_dir ~/Downloads/  "
+    command_A = "manim  -l  -c '#2B2B2B' --video_dir ~/Downloads/  "
     command_B = module_name +" " +"lllll"
     os.system(command_A + command_B)
