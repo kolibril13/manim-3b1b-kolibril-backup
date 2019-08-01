eb7b9cfc37718dcd0e791926708c825961763760
kolibril13 <jan-hendrik.mueller@gmx.net>
2019-08-01 11:54:39 +0200

    # option with UpdateFromFunc is now also fine!

--- hendrik_active/Image_Processing/poisson_noise_and_snr/k_Space/test03_Valuetracker_minimal_with_add_updater.py
+++ hendrik_active/Image_Processing/poisson_noise_and_snr/k_Space/test03_Valuetracker_minimal_with_add_updater.py
@@ -1,19 +1,18 @@
 from manimlib.imports import *
 
 class lllll(Scene):
-    def get_dot(self,num_tracker):
-        print(num_tracker.get_value())
-        return Dot().shift(RIGHT*0.01 *num_tracker.get_value())
+    def get_dot(self,num):
+        print(num)
+        return Dot().shift(LEFT*0.01 *num)
     def construct(self):
         tick_start=0
         tick_end=100
         val_tracker= ValueTracker(tick_start)
-        dot_disp= self.get_dot(val_tracker)
+        dot_disp= self.get_dot(val_tracker.get_value())
+
+        dot_disp.add_updater(lambda x: x.become(self.get_dot(val_tracker.get_value())))
         self.add(dot_disp)
-        self.play(
-            UpdateFromFunc(dot_disp, lambda mob: mob.become(self.get_dot(val_tracker))),
-            val_tracker.set_value,tick_end
-        )
+        self.play(val_tracker.set_value, tick_end, rate_func= linear)
         self.wait()
        
     
