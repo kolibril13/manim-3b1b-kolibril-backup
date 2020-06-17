899fb63bd01e6c64c6399ce37e9d16fd30ae86b5
kolibril13 <jan-hendrik.mueller@gmx.net>
2020-06-17 14:01:01 +0200

    #new_master

--- hendrik_active/math_material/test.py
+++ hendrik_active/math_material/test.py
@@ -25,7 +25,7 @@ class ExampleScene(Scene):
         self.wait(1)
 
 if __name__ == "__main__":
-    module_name = os.path.basename(__file__)
-    command_A = "manim -p  -c '#2B2B2B' --video_dir ~/Downloads/  "
-    command_B = module_name + " " + "ExampleScene"
+    manim_main = Path.home() / "projects/manim/manim.py"
+    command_A =   f"{manim_main}  -s -p -c '#2B2B2B' --video_dir ~/Downloads/  "
+    command_B = f"{Path(__file__).resolve()}   "
     os.system(command_A + command_B)
